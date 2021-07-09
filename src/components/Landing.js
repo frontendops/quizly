6fa13c6edef2c366b3b1b47cfd7b5b85d76108d8
Alejandro Sierra <alexsierra1598@gmail.com>
2021-07-08 21:54:30 -0400

    Translate code for landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,7 +1,21 @@
+import './styles/home.css';
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Landing = () => {
-    return <h1>This is your landing page</h1>
+    return (
+        <div className="background-gradient">
+            <div>
+            <h1 className="logo">Quizzly</h1>
+            
+            <div className="btn-container">
+                <Link to="/create" >
+                    <button type="button" className="btn btn-primary">create quiz</button>
+                </Link>
+            </div>
+            </div>
+        </div>
+    )
 }
 
 export default Landing
